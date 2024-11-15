b808df3097b6cf74c14adb943d8c509023453285
Angel Alfredo Urrego <anvixoli@gmail.com>
2024-11-15 17:27:39 -0500

    vue en su maximo nivel

---
+++ src/components/vue/Notificacion.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+let qty =ref(0);
+function add (){
+    qty.value++
+    console.log(qty)
+}
+function resta(){
+    qty.value--
+    console.log(qty)
+}
+</script>
+<template>
+    <div class="bg-purple-300 max-w-80 p-6 mx-auto rounded">
+        <div 
+        v-if="qty > 0"
+        class="bg-red-500 text-white size-6 rounded-full flex justify-center items-center font-bold" :class="{'bg-orange-500': qty > 3}">
+        
+            {{ qty }}
+
+        </div>
+        <div v-else >
+            Nadie te visita :(
+
+        </div>        
+        <div 
+        @click="add"
+        class="bg-blue-400 text-white rounded size-6 flex justify-center items-center">
+           
+            +
+
+        </div>
+        <div 
+        
+        @click="resta"
+        class="bg-blue-400 text-white rounded size-6 flex justify-center items-center">
+           
+            -
+
+        </div>
+
+    </div>
+</template>
